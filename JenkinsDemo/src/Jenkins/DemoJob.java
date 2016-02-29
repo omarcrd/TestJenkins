05f53abaa7ee39c13db67407c3ccba2df2254aad
omarcrd <omarcrd77@gmail.com>
2016-02-29 13:00:15 -0600

    line to get page title and display on the output console

--- JenkinsDemo/src/Jenkins/DemoJob.java
+++ JenkinsDemo/src/Jenkins/DemoJob.java
@@ -12,6 +12,9 @@ public class DemoJob {
 		WebDriver driver = new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.get("www.google.com");
+		
+		String PageTitle = driver.getTitle();
+		System.out.println("This is the page title:" + PageTitle);
 			
 		System.out.println("Hello atuomation tester your program ran sucesfully");
 		Thread.sleep(4000);
