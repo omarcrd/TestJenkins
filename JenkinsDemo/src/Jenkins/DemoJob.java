ec0fd779426d82f8feae952fabecb0e0e8cfb276
omarcrd <omarcrd77@gmail.com>
2016-03-03 11:37:58 -0600

    This is a test commit process

--- JenkinsDemo/src/Jenkins/DemoJob.java
+++ JenkinsDemo/src/Jenkins/DemoJob.java
@@ -16,6 +16,7 @@ public class DemoJob {
 		String PageTitle = driver.getTitle();
 		System.out.println("This is the page title:" + PageTitle);
 		System.out.println("This is a second commit changes");
+		System.out.println("Changes on March 03 11:20am");
 			
 		System.out.println("Hello atuomation tester your program ran sucesfully");
 		Thread.sleep(4000);
