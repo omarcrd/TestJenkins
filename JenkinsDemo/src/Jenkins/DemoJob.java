47923e5149fae517df0ea3d64228486a4aeb01ab
omarcrd <omarcrd77@gmail.com>
2016-03-07 10:07:19 -0600

    Commit changes on DemoJob.java on Monday March 7 10:07am aprox

--- JenkinsDemo/src/Jenkins/DemoJob.java
+++ JenkinsDemo/src/Jenkins/DemoJob.java
@@ -16,7 +16,7 @@ public class DemoJob {
 		String PageTitle = driver.getTitle();
 		System.out.println("This is the page title:" + PageTitle);
 		System.out.println("This is a second commit changes");
-		System.out.println("Changes on March 03 11:45am second test");
+		System.out.println("Changes on March 03 11:46am second test for push");
 			
 		System.out.println("Hello atuomation tester your program ran sucesfully");
 		Thread.sleep(4000);
